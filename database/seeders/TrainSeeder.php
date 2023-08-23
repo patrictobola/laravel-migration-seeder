c844e25cdc5d0b3c0db0951f00871e6a47a033f4
patrictobola <tobola92@gmail.com>
2023-08-23 15:29:25 +0200

    fake Generator in trainseed

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Train;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -11,8 +12,12 @@ class TrainSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $new_train = new Train();
+
+        $new_train->$faker->words(3, true);
+
+        $new_train->save();
     }
 }

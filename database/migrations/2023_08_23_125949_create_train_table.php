912cb054291c5220a20fd319a364b719fef4cc95
patrictobola <tobola92@gmail.com>
2023-08-23 15:15:20 +0200

    created table train and seeder

--- database/migrations/2023_08_23_125949_create_train_table.php
+++ database/migrations/2023_08_23_125949_create_train_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('train', function (Blueprint $table) {
             $table->id();
+            $table->string('agency_name');
+            $table->string('from_station');
+            $table->string('to_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->smallInteger('train_number');
+            $table->tinyInteger('number_of_cabs');
+            $table->boolean('delay');
+            $table->boolean('cancelled');
             $table->timestamps();
         });
     }
---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
